11942c2a3b1eb51a6479bf1efb8314b069a24252
yossefmahmoud1 <163425440+yossefmahmoud1@users.noreply.github.com>
2025-02-09 23:18:54 +0200

    Update useLocalStorage.js

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,1 +1,17 @@
 
+import { useState } from 'react';
+
+function useLocalStorage(key, initialValue) {
+  const storedValue = localStorage.getItem(key);
+
+  const [storedData, setStoredData] = useState(storedValue ? JSON.parse(storedValue) : initialValue);
+
+  const setValue = (value) => {
+    setStoredData(value);
+    localStorage.setItem(key, JSON.stringify(value)); // Save value in localStorage
+  };
+
+  return [storedData, setValue];
+}
+
+export default useLocalStorage;
