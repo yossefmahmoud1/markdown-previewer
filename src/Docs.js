4f2ce75633341f83ddd891a866df2d94e772e62c
yossefmahmoud1 <163425440+yossefmahmoud1@users.noreply.github.com>
2025-02-09 23:17:56 +0200

    Update Docs.js

--- src/Docs.js
+++ src/Docs.js
@@ -1,1 +1,29 @@
 
+import React, { useEffect, useState } from 'react';
+
+const Docs = () => {
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.markdownguide.org/api/v1/')
+      .then((response) => response.json())
+      .then((data) => setDocs(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Markdown Docs</h2>
+      <ul>
+        {docs.map((doc) => (
+          <li key={doc.id}>
+            <a href={doc.url} target="_blank" rel="noopener noreferrer">
+              {doc.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Docs;
